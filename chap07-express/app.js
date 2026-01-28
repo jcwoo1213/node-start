a7ad7ef0396f595d6a14b73f56f4ffc812a9f3ea
jcwoo1213 <jcwoo1213@gmail.com>
2026-01-28 17:53:30 +0900

    비밀번호 암호화 beacon 확인할것

--- chap07-express/app.js
+++ chap07-express/app.js
@@ -1,16 +1,12 @@
 const express = require("express");
-const app = express(); //인스턴스 생성
 const fs = require("fs");
 const multer = require("multer");
-const SERVER_PORT = 3000;
 const sampleRoute = require("./routes/sample.routes.js");
 const path = require("path");
 const pool = require("./db.js");
-//라우팅.url :실행함수
-app.use("/sample", sampleRoute);
-app.use(express.static("public"));
-// app.use(express.static("images"));
-//multer 설정
+const encoder = require("./crypto.js");
+const app = express(); //인스턴스 생성
+const SERVER_PORT = 3000;
 const storage = multer.diskStorage({
   //저장경로
   destination: (req, file, cb) => {
@@ -25,10 +21,37 @@ const storage = multer.diskStorage({
     cb(null, name + "_" + Date.now() + ext);
   },
 });
-
 const upload = multer({ storage }); //multer인스턴스
+
+//라우팅.url :실행함수
+app.use("/sample", sampleRoute);
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(express.static("images"));
+//multer 설정
+
 app.get("/", (req, res) => {
   res.send("첫페이지");
+});
+app.post("/upload", upload.single("user_img"), (req, res) => {
+  // console.log(req.body);
+  // console.log(req.file);
+  res.json({
+    user_name: req.body.user_name,
+    age: req.body.age,
+    file: req.file.filename,
+  });
+});
+app.get("/getMember", async (req, res) => {
+  const query = `select user_id,user_name,user_img from member`;
+  try {
+    const [rows, result] = await pool.query(query);
+    res.json(rows);
+  } catch (error) {
+    console.log(error);
+    res.json(error);
+  }
 });
 app.get("/test/:msg", (req, res) => {
   fs.writeFile("./test.txt", req.params.msg, "utf-8", (err, buf) => {
@@ -51,29 +74,91 @@ app.get("/read", (req, res) => {
     res.send(data);
   });
 });
-app.post("/upload", upload.single("user_img"), (req, res) => {
-  console.log(req.body);
-  console.log(req.file);
-  res.json({
-    user_name: req.body.user_name,
-    age: req.body.age,
-    file: req.file.filename,
-  });
+
+app.post("/create", upload.single("user_img"), async (req, res) => {
+  // console.log(req.body);
+
+  const { user_id, user_pw, user_name } = await req.body;
+  const user_img = req.file ? req.file.filename : null;
+
+  const { password, salt } = await encoder.createPassword(user_pw);
+  // console.log(password, salt);
+  const query = `insert into member (user_id,user_pw,user_name,user_img,salt) values(?,?,?,?,?);`;
+  try {
+    let result = await pool.query(query, [
+      user_id,
+      password,
+      user_name,
+      user_img,
+      salt,
+    ]);
+    res.json({ retCode: "Success" });
+  } catch (error) {
+    console.log(error);
+    const ufile = path.join(__dirname, "public/images", user_img);
+    fs.unlink(ufile, (err) => {
+      console.log(err);
+    });
+    res.json({ retCode: "NG", retMsg: error.sqlMessage });
+  }
 });
 
-app.post("/create", upload.single("user_img"), async function (req, res) {
-  console.log(req.body);
-  const { user_id, user_pw, user_name } = req.body;
-  const user_img = req.file ? req.file.filename : null;
-  const query = `insert into member (user_id,user_pw,user_name,user_img) values(?,?,?,?);`;
-  let result = await pool.query(query, [user_id, user_pw, user_name, user_img]);
-  res.json(result);
+app.post("/login", async (req, res) => {
+  const { login_id, login_pw } = req.body;
+  // console.log(req.body);
+  const query = `select * from member where user_id =?`;
+  try {
+    const [rows, result] = await pool.query(query, [login_id]);
+    if (!rows.length) {
+      res.json({ retCode: "Fail", retMsg: "아이디가 없습니다" });
+      return;
+    }
+    console.log(rows[0]);
+    const { user_id, user_pw, salt } = rows[0];
+    // console.log(user_pw, salt);
+    const check = await encoder.checkPassword(salt, login_pw, user_pw);
+    // console.log(check);
+    if (check) {
+      res.json({
+        retCode: "Success",
+        name: rows[0].user_name,
+        role: rows[0].responsibility,
+        id: rows[0].user_id,
+      });
+    } else {
+      res.json({ retCode: "Fail", retMsg: "비밀번호를 다시 확인해 주세요" });
+    }
+  } catch (error) {
+    res.json({ retCode: "Error", retMsg: error });
+  }
 });
-app.get("/upload", (req, res) => {
-  console.clear();
-  console.log(req.url);
-  res.send();
+app.delete("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = "delete from member where user_id = ?";
+  try {
+    const [filename, temp] = await pool.query(
+      "select user_img from member where user_id =?",
+      [id],
+    );
+    const file = filename[0].user_img;
+    if (file) {
+      const ufile = path.join(__dirname, "public/images", file);
+      fs.unlink(ufile, (err) => {
+        console.log(err);
+      });
+    }
+    const result = await pool.query(query, [id]);
+    res.json({ retCode: "Success" });
+  } catch (error) {
+    console.log(error);
+    res.json({ retCode: "error", retMsg: error });
+  }
 });
 app.listen(SERVER_PORT, () => {
   console.log(`서버실행 http://localhost:${SERVER_PORT}`);
+});
+
+app.post("/beacon-test", express.raw({ type: "*/*" }), (req, res) => {
+  console.log("beacon 수신:", req.body.toString());
+  res.status(204).end();
 });
